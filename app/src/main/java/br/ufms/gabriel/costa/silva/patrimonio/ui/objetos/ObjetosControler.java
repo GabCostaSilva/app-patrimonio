c0b1603bbdc541073528d02d92307353397ac40c
GabCostaSilva <gabcostadeveloper@gmail.com>
2023-10-25 00:01:13 -0400

    fix objeto fragment_dashboard.xml

---
+++ app/src/main/java/br/ufms/gabriel/costa/silva/patrimonio/ui/objetos/ObjetosControler.java
@@ -0,0 +1,12 @@
+package br.ufms.gabriel.costa.silva.patrimonio.ui.objetos;
+
+import android.view.View;
+
+public interface ObjetosControler {
+
+    void excluirObjeto(View view);
+
+    void salvarObjeto(View view);
+
+    void voltar(View view);
+}
